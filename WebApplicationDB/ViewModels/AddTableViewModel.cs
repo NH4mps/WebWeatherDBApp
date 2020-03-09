06629312548ecfd36316b427f0a8e3f76f3ae1d8
NH4mps <NH4mps@users.noreply.github.com>
2020-03-09 05:29:19 +0300

    Revert "Revert "Added multiple uploading""
    This reverts commit 08860c6cc42b8cc2a7a1a15bbd41210b892b7589.

---
+++ WebApplicationDB/ViewModels/AddTableViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationDB.ViewModels
+{
+    public class AddTableViewModel
+    {
+        public List<string> statusStringList { get; set; }
+        public List<string> colorList { get; set; }
+    }
+}
